ca3344cb2fcfaa2060e300e262e1e34f44db3c50
greg <greg.sidier@gmail.com>
2017-10-23 13:42:16 +0100

    add test

--- nn.py
+++ nn.py
@@ -1,4 +1,5 @@
-from numpy import exp, log, where, dot, sum, hstack
+from numpy import exp, log, where, dot, sum, hstack, abs, transpose
+from numpy.random import randn
 
 logistic = (lambda x: 1/(1+exp(-x)), lambda x,f: f*(1-f))
 relu = (lambda x: where(x>0, x, 0), lambda x,f: where(x>0, 1., 0.))
@@ -35,5 +36,36 @@ def sgd(W, f, loss, alpha, xtrain, ytrain):
     return W, g
 
 def randomweights(dims):
-    W = [ randn(rows + 1, cols) for (rows, cols) in zip(dims[:-1], dims[1:]) ]
+    return [ randn(rows + 1, cols) for (rows, cols) in zip(dims[:-1], dims[1:]) ]
 
+if __name__ == '__main__':
+
+# test gradient calc
+
+	eps = 1e-2
+	
+	dims = [5, 20, 2]
+	W = randomweights(dims)
+	f = [ logistic, logistic, logistic ]
+	
+	xin = randn(dims[0])
+	X, z = fwprop(xin, W, f)
+
+	target = squareloss(randn(dims[-1]))
+	g = backprop(X, W, f, z, target)
+	
+	for k, (Wk, gk) in enumerate(zip(W, g)):
+		for i in xrange(Wk.shape[0]):
+			for j in xrange(Wk.shape[1]):
+				Wup = [ _ for _ in W ]
+				Wup[k] = Wk.copy()
+				Wup[k][i, j] += eps
+				Xup, _ = fwprop(xin, Wup, f)
+				Wdn = [ _ for _ in W ]
+				Wdn[k] = Wk.copy()
+				Wdn[k][i, j] -= eps
+				Xdn, _ = fwprop(xin, Wdn, f)
+				dloss_dWij = (target[0](Xup[-1]) - target[0](Xdn[-1])) / (2*eps)
+				assert(abs(dloss_dWij - gk[i, j]) < 1e-4)
+				
+	print "All tests passed."
